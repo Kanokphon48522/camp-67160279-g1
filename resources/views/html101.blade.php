dab5fe55f19afdfaed40da491a7be9493005b3f4
67160176 <67160176@go.buu.ac.th>
2025-12-04 19:06:55 +0700

    workshop html

---
+++ resources/views/html101.blade.php
@@ -0,0 +1,35 @@
+<!-- file : resources/views/html101.blade.php -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ส่วนหัวของ html</title>
+        <link rel="stlyesheet" href="css/bootstrap.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Playpen+Sans+Thai:wght@100..800&display=swap" rel="stylesheet">
+        <style>
+            body {
+                font-family: "Playpen Sans Thai", cursive;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container mt-4">
+            <h1> workshop #HTML - FORM</h1>
+            <form>
+                <div class="row">
+                    <div class="col-sm-12 col-md-4">
+                        ชื่อ
+                    </div>
+                    <div class="col">
+                        <input id="fname" class="form-control"> 
+                    </div>
+                </div>
+        </form>
+        
+        </div>
+</body>
+
+</html>
+
+</ead>
